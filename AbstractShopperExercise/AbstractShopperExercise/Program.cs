83adda02884d996008deda922900855954ad5ba4
KhaeLim <khaelimr@gmail.com>
2020-05-26 12:24:45 +0100

    Shopper Exercises)(Inheritance/Abstract)(

---
+++ AbstractShopperExercise/AbstractShopperExercise/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractShopperExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regular me = new Regular("Khae", 1, 22, "Male");
+            Mystery notMe = new Mystery("Over There", 1, 22, "Unknown");
+
+            Console.WriteLine(me.DisplayShopperInfo());
+            Console.WriteLine(notMe.DisplayShopperInfo());
+            Console.ReadKey();
+        }
+    }
+
+    public abstract class Shopper
+    {
+        public int id { get; set; }
+        public int age { get; set; }
+        public String gender { get; set; }
+        public abstract String DisplayShopperInfo();
+    }
+    public class Regular : Shopper
+    {
+        public String name { get; set; }
+        public Regular(String name, int id, int age, String gender)
+        {
+            this.name = name;
+            this.id = id;
+            this.age = age;
+            this.gender = gender;
+        }
+        public override String DisplayShopperInfo()
+        {
+            return "Name: " + name + ", " + "ID: " + id + ", Age: " + age + ", Gender: " + gender;
+        }
+    }
+    public class Mystery : Shopper
+    {
+        public String area { get; set; }
+        public Mystery(String area, int id, int age, String gender)
+        {
+            this.area = area;
+            this.id = id;
+            this.age = age;
+            this.gender = gender;
+        }
+        public override String DisplayShopperInfo()
+        {
+            return "Area: " + area + ", " + "ID: " + id + ", Age: " + age + ", Gender: " + gender;
+        }
+    }
+
+}
---
+++ ShopperExercise/ShopperExercise/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopperExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shopper khae = new Shopper(1, 23, "Male");
+            Regular me = new Regular("Khaelim", 2, 33, "MaleIsh");
+            Mystery notMe = new Mystery("Electrical", 3, 40, "Female");
+
+            Console.WriteLine(khae.DisplayShopperInfo());
+            Console.WriteLine(me.DisplayShopperInfo());
+            Console.WriteLine(notMe.DisplayShopperInfo());
+            Console.ReadKey();
+        }
+    }
+
+    public class Shopper
+    {
+        public int id { get; set; }
+        public int age { get; set; }
+        public String gender { get; set; }
+        public Shopper(int id, int age, String gender)
+        {
+            this.id = id;
+            this.age = age;
+            this.gender = gender;
+        }
+        public virtual String DisplayShopperInfo()
+        {
+            return "ID: "+id+", Age: "+age+", Gender: "+gender;
+        }
+    }
+    public class Regular : Shopper
+    {
+        public String name { get; set; }
+        public Regular(String name,int id, int age, String gender):base(id, age, gender)
+        {
+            this.name = name;
+        }
+        public override String DisplayShopperInfo()
+        {
+            return "Name: "+name+", "+base.DisplayShopperInfo();
+        }
+    }
+    public class Mystery : Shopper
+    {
+        public String area { get; set; }
+        public Mystery(String area, int id, int age, String gender) : base(id, age, gender)
+        {
+            this.area = area;
+        }
+        public override String DisplayShopperInfo()
+        {
+            return "Area: " + area + ", " + base.DisplayShopperInfo();
+        }
+    }
+
+
+}
